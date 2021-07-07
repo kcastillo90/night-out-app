495f11ba2f95d16a4950eb327d343bc6660fe3f0
Kyle Castillo <kyle.castillo90@gmail.com>
2021-07-07 15:07:43 -0400

    some styling completed

--- app.js
+++ app.js
@@ -9,35 +9,35 @@ $(() => {
       return
     }
 
-    $.ajax({
-      async: true,
-      crossDomain: true,
-      url: `https://documenu.p.rapidapi.com/restaurants/zip_code/${$zip}?size=30&fullmenu=false&page=2`,
-      method: "GET",
-      headers: {
-        "x-api-key": "a5da2fb501238f04a7faea97564973b4",
-        "x-rapidapi-key": "5afdaea20bmsh0c8158b162825e2p1ce46cjsnd2191c7b8519",
-        "x-rapidapi-host": "documenu.p.rapidapi.com"
-        }
-      }).then(
-        (results) => {
-          console.log(results)
-
-          console.log(results.data);
-
-          for(i = 0; i < results.data.length; i++)  {
-            const $restaurantDiv = $('<div>').attr(`id`, `${results.data[i].restaurant_name}`)
-            const $restaurant = $('<dt>').attr(`id`, `${results.data[i].restaurant_name}`).text(`${results.data[i].restaurant_name}`)
-            const $restaurantDetails = $('<dl>')
-              const $cuisine = $('<dd>').text(`${results.data[i].cuisines}`).appendTo($restaurantDetails)
-              const $priceRange = $('<dd>').text(`${results.data[i].price_range}`).appendTo($restaurantDetails)
-              const $hours = $('<dd>').text(`${results.data[i].hours}`).appendTo($restaurantDetails)
-              const $website = $('<dd>').text(`${results.data[i].restaurant_website}`).appendTo($restaurantDetails)
-              const $phone = $('<dd>').text(`${results.data[i].restaurant_phone}`).appendTo($restaurantDetails)
-              const $address = $('<dd>').text(`${results.data[i].address.formatted}`).appendTo($restaurantDetails)
-            $restaurantDiv.append($restaurant, $restaurantDetails)
-            $('#restaurant_carousel').append($restaurantDiv)
-          }
+    // $.ajax({
+    //   async: true,
+    //   crossDomain: true,
+    //   url: `https://documenu.p.rapidapi.com/restaurants/zip_code/${$zip}?size=30&fullmenu=false&page=2`,
+    //   method: "GET",
+    //   headers: {
+    //     "x-api-key": "a5da2fb501238f04a7faea97564973b4",
+    //     "x-rapidapi-key": "5afdaea20bmsh0c8158b162825e2p1ce46cjsnd2191c7b8519",
+    //     "x-rapidapi-host": "documenu.p.rapidapi.com"
+    //     }
+    //   }).then(
+    //     (results) => {
+    //       console.log(results)
+    //
+    //       console.log(results.data);
+    //
+    //       for(i = 0; i < results.data.length; i++)  {
+    //         const $restaurantDiv = $('<div>').attr(`id`, `${results.data[i].restaurant_name}`)
+    //         const $restaurant = $('<dt>').attr(`id`, `${results.data[i].restaurant_name}`).text(`${results.data[i].restaurant_name}`)
+    //         const $restaurantDetails = $('<dl>')
+    //           const $cuisine = $('<dd>').text(`${results.data[i].cuisines}`).appendTo($restaurantDetails)
+    //           const $priceRange = $('<dd>').text(`${results.data[i].price_range}`).appendTo($restaurantDetails)
+    //           const $hours = $('<dd>').text(`${results.data[i].hours}`).appendTo($restaurantDetails)
+    //           const $website = $('<dd>').text(`${results.data[i].restaurant_website}`).appendTo($restaurantDetails)
+    //           const $phone = $('<dd>').text(`${results.data[i].restaurant_phone}`).appendTo($restaurantDetails)
+    //           const $address = $('<dd>').text(`${results.data[i].address.formatted}`).appendTo($restaurantDetails)
+    //         $restaurantDiv.append($restaurant, $restaurantDetails)
+    //         $('#restaurant_carousel').append($restaurantDiv)
+    //       }
           // Carousel next and previous buttons:
           let $currentRestaurant = 0
           // will be index of restaurant array once it's generated (starting at 0)
@@ -75,7 +75,7 @@ $(() => {
           })
 
 
-      })
+      // })
 
 
 
